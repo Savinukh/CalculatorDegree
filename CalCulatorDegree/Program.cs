e2daea887a6dc009f46a4f7709f190f26baa31a8
Left_Tapochek <serregasavinykh15@gmail.com>
2020-11-16 02:54:49 +0500

    Создание метода: перевод градусов Фаренгейт в Цельсия

--- CalCulatorDegree/Program.cs
+++ CalCulatorDegree/Program.cs
@@ -7,6 +7,7 @@ namespace CalCulatorDegree
         static void Main(string[] args)
         {
             MethodCelsiusToFahrenheit();
+            MethodFahrenheitToCelsius();
         }
         static void MethodCelsiusToFahrenheit()
         {
@@ -14,8 +15,18 @@ namespace CalCulatorDegree
             int temperature = int.Parse(Console.ReadLine());
             double result = 0;
             result = temperature * 1.8 + 32;
-            Console.WriteLine("{0}°С перевод в Френгейт: {1}°F", temperature, result);
+            Console.WriteLine("{0}°С перевод в Фаренгейт: {1}°F", temperature, result);
             Console.ReadKey();
         }
+        static void MethodFahrenheitToCelsius()
+        {
+            Console.Write("Введите температуру по шкале Фаренгейта:\t");
+            int temperature = int.Parse(Console.ReadLine());
+            double result = 0;
+            result = (temperature - 32) / 1.8;
+            Console.WriteLine("{0}°F перевод в Френгейт: {1}°С", temperature, result);
+            Console.ReadKey();
+        }
+        
     }
 }
